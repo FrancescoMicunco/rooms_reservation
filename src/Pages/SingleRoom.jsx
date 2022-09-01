c3f22a6f03fc680e611f3bacc07d054e25b5dc03
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-09-01 20:41:55 +0200

    SinglePage

--- src/Pages/SingleRoom.jsx
+++ src/Pages/SingleRoom.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import { useParams } from 'react-router-dom'
+import room from "../data/rooms"
 
 const SingleRoom = () => {
-    let { name } = useParams()
+
+    let sectionStyle = {
+        backgroundImage: "url('https://picsum.photos/200/300')",
+        backgroundPosition: 'top',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        minHeight: '100vh'
+    }
+    let params = useParams()
+    let roomSelected = room?.find(r => r.name === params.name)
+
     return (
 
-        < h1 > {name} Description</h1 >
+        <div>
+
+            <div style={{
+                backgroundImage: "url('https://picsum.photos/500/200')",
+                position: 'relative',
+                backgroundPosition: 'top',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'contain',
+                minHeight: '50vh',
+                paddingTop: '15%'
+
+            }}>
+                <div style={{ position: 'absolute', background: 'rgba(0,0,0,0.5)', top: '15%', width: '100%' }}>
+                    < h1 style={{ color: 'white', border: 'solid 8px white', margin: '10% 30%', padding: '3% 0' }}> {params.name} Room</h1 >
+                </div>
+            </div>
+
+            <div>
+                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Doloremque quia hic voluptatibus quaerat cum aliquam, voluptas dolores ut explicabo distinctio!
+                </h3>
+            </div>
+
+
+        </div >
+
     )
 
 
