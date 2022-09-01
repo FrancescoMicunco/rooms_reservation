8e212c471ab374247ddb9e46b09d6fb109eb2a17
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-09-01 11:06:12 +0200

    customers DB

---
+++ src/data/customers.js
@@ -0,0 +1,19 @@
+const Customers = [{
+        name: "Albert",
+        paxNum: 4,
+    },
+    {
+        name: "Jordan",
+        paxNum: 2,
+    },
+    {
+        name: "Sorian",
+        paxNum: 3,
+    },
+    {
+        name: "Serjei",
+        paxNum: 5,
+    },
+];
+
+export default Customers;
