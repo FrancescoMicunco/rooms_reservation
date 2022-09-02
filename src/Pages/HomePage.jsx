d61f82dbd74cf738fcd781fa75622c510293f994
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-09-02 13:26:16 +0200

    homePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../styles/homePage.css";
 import { useNavigate, Link } from "react-router-dom"
+import Button from '@mui/material/Button'
+
 
 const HomePage = () => {
 
@@ -11,7 +13,7 @@ const HomePage = () => {
             <div className="roomList">
                 <h2>Benvenuto nel sito della nostra azienda agrituristica</h2>
                 <h3>Scopri di più sulla nostra attività e visita le nostre soluzioni di ospitalità</h3>
-                <button onClick={() => { navigate("/rooms") }}>Vistita la nostra Room List</button>
+                <Button variant="contained" onClick={() => { navigate("/rooms") }}>Room List</Button>
             </div>
             <div className="lastMinute">
                 <h3> Last Minute Offer </h3>
