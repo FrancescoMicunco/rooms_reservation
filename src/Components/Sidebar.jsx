b42cdd4fe0798400db9eda5fa2a9e8461a293315
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-09-02 17:53:19 +0200

    sidebar

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../styles/dashBoard.css'
+import BookOnlineIcon from '@mui/icons-material/BookOnline';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+            <table>
+                <tbody>
+                    <tr><td><BookOnlineIcon style={{ fontSize: '30px' }} /></td><td className='textSidebar'>Reservation</td></tr>
+                    <tr><td><HowToRegIcon style={{ fontSize: '30px' }} /></td><td className='textSidebar'>Registration</td></tr>
+                    <tr><td><ManageAccountsIcon style={{ fontSize: '30px' }} /></td><td className='textSidebar'>Management</td></tr>
+                    <tr><td><SettingsIcon style={{ fontSize: '30px' }} /></td><td className='textSidebar'>Settings</td></tr>
+                </tbody>
+            </table>
         </div>
     );
 }
