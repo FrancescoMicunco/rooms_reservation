ad8dbd667d2322b4de6cc47fd5c8896b3a4c932e
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-10-02 09:47:58 +0200

    registration form

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -6,11 +6,12 @@ import { useLocation } from 'react-router-dom'
 const Registration = () => {
     const location = useLocation();
     const data = location.state.record
+
     console.log("Location from stickyTable", data._id);
     return (
         <>
             < h1 > Reservation form</h1 >
-            <p>{location.state.customerName}</p>
+            <p>{data.customerName}</p>
         </>
     )
 
